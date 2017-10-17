f842d9ca40f33f208cf341dd327ed06375a76afb
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-17 06:22:32 -0400

    SUMMARY: Adds two buttons and associated onClick actions. REMARKS: From https://www.coursera.org/learn/android-app/
    FILE: MainActivity.java
      ADDS: toDo() method to handle actions when the buttons are clicked.
      This method uses pop up messages and modifies visibility of a view
      element (button).
    
    FILE: activity_main.xml
      ADDS: Two buttons and colors them.

--- app/src/main/java/com/example/tarajano/block01app/MainActivity.java
+++ app/src/main/java/com/example/tarajano/block01app/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.tarajano.block01app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonBlue, buttonPink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonPink = (Button) findViewById(R.id.button_pinkPanther);
+        buttonBlue = (Button) findViewById(R.id.button_blueInvisible);
+    }
+
+    public void toDO(View v){
+
+        if(v.equals(buttonBlue)){
+            // Invisible Man' stuff
+            v.setVisibility(View.INVISIBLE);
+            Toast.makeText(getApplicationContext(),"Upss! The Invisible Man is gone.", Toast.LENGTH_SHORT).show();
+        }
+
+        if(v.equals(buttonPink)){
+            // Panther's stuff
+            // Pop-up
+            Toast.makeText(getApplicationContext(),"Ohh! Ohh! What TO DO?", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
