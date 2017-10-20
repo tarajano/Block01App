65c011d24ca51ca7d5f275ad4d783e6d8172f6e7
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-20 18:57:39 -0400

    SUMMARY: Creating a new button (greenButton) directly from java code.
    FILE:MainActivity

--- app/src/main/java/com/example/tarajano/block01app/MainActivity.java
+++ app/src/main/java/com/example/tarajano/block01app/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.tarajano.block01app;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button buttonBlue, buttonPink;
+    Button buttonBlue, buttonPink, buttonGreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +19,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         buttonPink = (Button) findViewById(R.id.button_pinkPanther);
         buttonBlue = (Button) findViewById(R.id.button_blueInvisible);
+
         // Preparing to set onClick actions
         buttonPink.setOnClickListener(this);
         buttonBlue.setOnClickListener(this);
+
+        // Creating a new button directly from code (not xml)
+        buttonGreen = new Button(getApplicationContext());
+        buttonGreen.setTextColor(Color.BLACK);
+        buttonGreen.setBackgroundColor(Color.WHITE);
+        buttonGreen.setText("Don't you fancy some green?");
+        RelativeLayout rlMainActivity = (RelativeLayout) findViewById(R.id.mainActivityLayout);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM , buttonGreen.getId());
+        rlMainActivity.addView(buttonGreen, params);
+        buttonGreen.setOnClickListener(this);
     }
 
     @Override
@@ -29,15 +46,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void toDO(View v){
 
         if(v.equals(buttonBlue)){
-            // Invisible Man' stuff
+            // Make button invisible
             v.setVisibility(View.INVISIBLE);
             Toast.makeText(getApplicationContext(),"Upss! The Invisible Man is gone.", Toast.LENGTH_SHORT).show();
         }
 
         if(v.equals(buttonPink)){
-            // Panther's stuff
             // Pop-up
             Toast.makeText(getApplicationContext(),"Ohh! Ohh! What TO DO?", Toast.LENGTH_SHORT).show();
+        }
+
+        if(v.equals(buttonGreen)){
+            // Pop-up
+            buttonGreen.setBackgroundColor(Color.GREEN);
+            buttonGreen.setText("Yes! We fancy some green here.");
         }
 
     }
